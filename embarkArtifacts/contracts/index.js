bf287652aec02cdf8f30bd09c59631513ec4281d
Brianspha <g14m1190@campus.ru.ac.za>
2019-06-17 15:44:35 +0200

    update

--- embarkArtifacts/contracts/index.js
+++ embarkArtifacts/contracts/index.js
@@ -1,5 +1,8 @@
 module.exports = {
 "SafeMath": require('./SafeMath').default,
+"FIFSRegistrar": require('./FIFSRegistrar').default,
+"ENSRegistry": require('./ENSRegistry').default,
+"Resolver": require('./Resolver').default,
 "BountyContract": require('./BountyContract').default,
 
 };
